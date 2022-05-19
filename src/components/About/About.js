4d73ed8856877c78aa2d3beeaa98381632f38128
Gautam <pbg2402@gmail.com>
2022-05-19 15:00:15 +0530

    Added: about me cards

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,3 +1,9 @@
+import {
+    InformationCircleIcon,
+    RefreshIcon,
+    CodeIcon,
+} from '@heroicons/react/outline'
+
 const About = () => {
     return (
         <div className="about bg-blue-500 w-full flex flex-col items-center py-[15rem] relative">
@@ -16,24 +22,128 @@ const About = () => {
                             to create products for business as well as for
                             consumer use.
                         </span>
-
-                        <span className="mt-6">
-                            I am a naturally curious and committed person who
-                            solves his problems, one at a time.
-                        </span>
                     </p>
                 </div>
             </div>
 
-            <div className="items-center w-[90%] space-y-5 lg:space-y-0 lg:justify-around bottom-[-10rem] lg:bottom-[-3rem] absolute flex flex-col lg:flex-row lg:space-x-5">
+            <div className="items-center w-[90%] space-y-5 lg:space-y-0 lg:justify-around bottom-[-110rem] lg:bottom-[-30rem] absolute flex flex-col lg:flex-row lg:space-x-5">
                 {/* about card birthday, hobbies etc */}
-                <div className="bg-white p-10 rounded-xl hover:border-none hover:shadow-xl transition ease-out duration-300 cursor-pointer border border-blue-500 flex flex-col items-center w-full">
-                    <h1 className="font-bold text-lg lg:text-2xl">About me</h1>
+                <div className="bg-white p-10 rounded-xl  transition ease-out duration-300 border border-blue-500 flex flex-col items-center w-full h-[40rem]">
+                    {/* header */}
+                    <div className="flex flex-col items-center space-y-5">
+                        <InformationCircleIcon className="h-10 text-blue-500" />
+                        <h1 className="font-bold text-lg lg:text-2xl">
+                            About me
+                        </h1>
+                    </div>
+
+                    {/* content */}
+                    <div className="flex flex-col items-center space-y-4">
+                        <p className="mt-6 text-center">
+                            I am a naturally curious and committed person who
+                            solves his problems, one at a time.
+                        </p>
+
+                        <div className="flex flex-col items-center text-center space-y-2">
+                            <p className="font-bold text-lg text-blue-500">
+                                More about myself:
+                            </p>
+                            <p className=" font-bold">
+                                Born on -{' '}
+                                <span className="font-normal">
+                                    24th February, 2001
+                                </span>
+                            </p>
+                            <p className=" font-bold">
+                                I am -{' '}
+                                <span className="font-normal">
+                                    21 years old
+                                </span>
+                            </p>
+                            <p className=" font-bold">
+                                Reach me -{' '}
+                                <span className="font-normal">
+                                    pbg2402@gmail.com
+                                </span>
+                            </p>
+                            <p className=" font-bold">
+                                Based in -{' '}
+                                <span className="font-normal">
+                                    Bangalore, India
+                                </span>
+                            </p>
+                            <p className=" font-bold">
+                                Currently pursuing -{' '}
+                                <span className="font-normal">
+                                    B-Tech at PES University
+                                </span>
+                            </p>
+                        </div>
+                    </div>
                 </div>
 
                 {/* what I do card */}
-                <div className="bg-white p-10 rounded-xl hover:border-none hover:shadow-xl transition ease-out duration-300 cursor-pointer border border-blue-500 flex flex-col items-center w-full">
-                    <h1 className="font-bold text-lg lg:text-2xl">What I do</h1>
+                <div className="bg-white h-[40rem] p-10 rounded-xl  transition ease-out duration-300 border border-blue-500 flex flex-col items-center w-full">
+                    {/* header */}
+                    <div className="flex flex-col items-center space-y-5">
+                        <RefreshIcon className="h-10 text-blue-500" />
+                        <h1 className="font-bold text-lg lg:text-2xl">
+                            What I do
+                        </h1>
+                    </div>
+
+                    {/* content */}
+                    <div className="flex flex-col items-center space-y-4">
+                        <p className="mt-6 text-center">
+                            I love building websites and learning new things. I
+                            also love working in a team
+                        </p>
+
+                        <div className="flex flex-col items-center text-center space-y-2">
+                            <p className="font-bold text-lg text-blue-500">
+                                Some of my hobbies:
+                            </p>
+                            <p>Coding</p>
+                            <p>Watching Movies</p>
+                            <p>Listening to music</p>
+                            <p>Gyming</p>
+                            <p>Gaming</p>
+                        </div>
+                    </div>
+                </div>
+
+                {/* languages I speak (dev stuff) */}
+                <div className="bg-white h-[40rem] p-10 rounded-xl transition ease-out duration-300 border border-blue-500 flex flex-col items-center w-full">
+                    {/* header */}
+                    <div className="flex flex-col items-center space-y-5">
+                        <CodeIcon className="h-10 text-blue-500" />
+                        <h1 className="font-bold text-lg lg:text-2xl">
+                            Tools I Use
+                        </h1>
+                    </div>
+
+                    {/* content */}
+                    <div className="flex flex-col items-center space-y-4">
+                        <p className="mt-6 text-center">
+                            I am a fast learner and learn by watching and
+                            collaborating with others
+                        </p>
+
+                        <div className="flex flex-col items-center text-center space-y-2">
+                            <p className="font-bold text-lg text-blue-500">
+                                Languages I speak:
+                            </p>
+                            <p>HTML</p>
+                            <p>CSS</p>
+                            <p>JavaScript</p>
+                            <p>ReactJS</p>
+                            <p>NextJS</p>
+                            <p>TailwindCSS</p>
+                            <p>Figma</p>
+                            <p>Sanity.io</p>
+                            <p>Git</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -2,8 +2,8 @@ import { Link } from 'react-scroll'
 
 const NavigationBar = () => {
     return (
-        <header className="flex text-black justify-between bg-transparent text-lg p-5">
-            <div className="text-blue-500 absolute right-0 py-2 px-5 rounded-full cursor-pointer border border-blue-500 mr-5 hover:bg-blue-500 hover:text-white transition ease-in-out duration-300">
+        <header className="flex text-black w-full bg-white shadow-lg justify-between p-5 bg-transparent">
+            <div className="text-blue-500 ml-auto py-2 px-5 rounded-full cursor-pointer border border-blue-500 mr-5 hover:bg-blue-500 hover:text-white transition ease-in-out duration-300">
                 <Link to="contact" duration={1000} smooth={true} offset={-60}>
                     <p>Let's connect</p>
                 </Link>
