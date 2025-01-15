54f1bde796191603d8e0497908f56d2b9fdf8f48
GautamPB <pbg2402@gmail.com>
2025-01-15 17:39:23 +0530

    Added: env variables

--- firebase.ts
+++ firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from '@firebase/firestore'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: 'AIzaSyD3Igc2ir0InxdEtqvk0CwuHntJ_8pptFY',
-    authDomain: 'portfolio-website-96167.firebaseapp.com',
-    projectId: 'portfolio-website-96167',
-    storageBucket: 'portfolio-website-96167.firebasestorage.app',
-    messagingSenderId: '181283685937',
-    appId: '1:181283685937:web:f9eeb0b02336701f3a7710',
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_APP_ID,
 }
 
 // Initialize Firebase
